adb2495499d4222a9a42724c2014b402b8fb9ddb
Wojciech Opara <72667136+Kosmit147@users.noreply.github.com>
2021-03-17 13:42:20 +0100

    Added button slots

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -6,10 +6,29 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->buttonRock, SIGNAL(clicked()), this, SLOT(onButtonRockClicked()));
+    connect(ui->buttonPaper, SIGNAL(clicked()), this, SLOT(onButtonPaperClicked()));
+    connect(ui->buttonScissors, SIGNAL(clicked()), this, SLOT(onButtonScissorsClicked()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::onButtonRockClicked()
+{
+
+}
+
+void MainWindow::onButtonPaperClicked()
+{
+
+}
+
+void MainWindow::onButtonScissorsClicked()
+{
+
 }
 
